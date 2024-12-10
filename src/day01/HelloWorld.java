6e96fcc4e86302eb6ac158ac5cfe1002060dfd2b
kryst <spluwacz@aol.com>
2024-12-10 15:14:01 -0600

    12.10.24 update

--- src/day01/HelloWorld.java
+++ src/day01/HelloWorld.java
@@ -4,11 +4,10 @@ public class HelloWorld {
 
     public static void main(String[] args) {
 
-        System.out.println("HelloWorld");
-        System.out.println("HelloWorld");
-        System.out.println("HelloWorld");
-        System.out.println("HelloWorld");
-        System.out.println("HelloWorld");
+        System.out.println("Hello World");
+        System.out.println("Hello Cydeo");
+        System.out.println("Hello Programmers");
+
     }
 
 
