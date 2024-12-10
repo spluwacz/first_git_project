00dccc52182e7ce22940bc86c39cb294cdc6555a
spluwacz <spluwacz@aol.com>
2024-12-10 15:09:14 -0600

    Update HelloWorld.java

--- src/day01/HelloWorld.java
+++ src/day01/HelloWorld.java
@@ -5,7 +5,10 @@ public class HelloWorld {
     public static void main(String[] args) {
 
         System.out.println("HelloWorld");
-
+        System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
     }
 
 
